592caf4af15e9993a4709183c14e0cdc8316ad35
joaoamsantos <40895780+joaoamsantos@users.noreply.github.com>
2018-07-06 17:45:50 -0400

    Update huellaReader.js

--- www/huellaReader.js
+++ www/huellaReader.js
@@ -3,11 +3,11 @@ function HuellaReader() {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
-HuellaReader.prototype.show = function(message, duration, successCallback, errorCallback) {
+HuellaReader.prototype.checkDevices = function(message, duration, successCallback, errorCallback) {
   var options = {};
   options.message = message;
   options.duration = duration;
-  cordova.exec(successCallback, errorCallback, 'HuellaReader', 'show', [options]);
+  cordova.exec(successCallback, errorCallback, 'HuellaReader', 'checkDevices', [options]);
 }
 
 // Installation constructor that binds ToastyPlugin to window
